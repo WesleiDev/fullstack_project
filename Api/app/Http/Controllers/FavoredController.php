44f2f861269a209a5245ada1c600d0896df8b7fe
Weslei <wesleiDev@hotmail.com>
2021-09-16 18:02:35 -0300

    Melhorias

--- Api/app/Http/Controllers/FavoredController.php
+++ Api/app/Http/Controllers/FavoredController.php
@@ -26,15 +26,6 @@ class FavoredController extends Controller
         }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -58,18 +49,7 @@ class FavoredController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
+     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
